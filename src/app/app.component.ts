8d5447e41767f0ee6dcb79150f27be6ff1f5e291
anilpathuri <anil.pathuri@walkingtree.tech>
2017-06-25 09:31:19 +0530

    done drag and drop.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+const URL = '';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+
+  public uploader: FileUploader = new FileUploader({ url: URL });
+  public imagesUrl: any = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  public hasBaseDropZoneOver: boolean = false;
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+
+  public onFileDrop(files: File[]) {
+    for (var i = 0; i < files.length; i++) {
+      this.readFile(files[i]);
+    }
+  }
+  readFile(file: File) {
+    var reader = new FileReader();
+    reader.onload = () => {
+      var dataURL = reader.result;
+      this.imagesUrl.push(dataURL);
+    };
+    reader.readAsDataURL(file);
+  }
+
+
 }
